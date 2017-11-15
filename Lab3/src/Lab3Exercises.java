9c3a7b8d3550cc08b59d87c919ca12607b3ced4f
4015b18 <4015b18@coco.ece.uprm.edu>
2017-11-15 18:22:21 -0400

    Added center alignment and changed the size of the frame

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		JFrame myFrame = new JFrame("Pablo Sepulveda");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setSize(200, 200);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
 	}
+	
 }
